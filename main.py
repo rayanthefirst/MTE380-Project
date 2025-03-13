4304d66d7847c281d865056394218a8ea97ae8e9
Rayan Ahmad <rayanthefirst@gmail.com>
2025-03-13 18:14:11 -0400

    servo

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ servo = AngularServo(2, min_angle=0, max_angle=180, min_pulse_width=0.0006, max_
 def move_servo_90():
     print("Moving servo to 90 degrees")
     servo.angle = 90
-    time.sleep(3)
+    sleep(3)
     # sleep(3)  # Allow time for the servo to reach the target position
     # servo.angle = 0  # Reset the servo to 0 degrees
     # sleep(3)
 # Move the servo
-# while True:
-move_servo_90()
+while True:
+    move_servo_90()
