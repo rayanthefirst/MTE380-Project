d429afdb5e2c74da8be775cef6cf0d6fef4494df
Rayan Ahmad <rayanthefirst@gmail.com>
2025-03-18 15:35:37 -0400

    Quick test

--- runFile.py
+++ runFile.py
@@ -1,8 +1,13 @@
 
 from camera.camera import Camera
+from threading import Thread
 cam = Camera(camera_id=0)
-cam.start_detection(display=True)
+cameraProcess = Thread(target=cam.start_detection, args=(True,))
 
+
+while True:
+    print(cam.angle)
+    print(cam.curr_error)
 
 
 
