1fbdb6d9c196921a0a8bdbf599b63cde816d2dd2
Rayan Ahmad <rayanthefirst@gmail.com>
2025-03-18 15:55:33 -0400

    Quick test

--- runFile.py
+++ runFile.py
@@ -4,7 +4,7 @@ from threading import Thread
 import time
 cam = Camera(camera_id=0)
 # cam.start_detection(display=True)
-cameraProcess = Thread(target=cam.start_detection, kwargs={"display": False})
+cameraProcess = Thread(target=cam.start_detection, args={"display": False})
 
 
 while True:
