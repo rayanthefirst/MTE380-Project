2603c01bbfbeaf9d8f3d92799a070aeecb7e11ac
Rayan Ahmad <rayanthefirst@gmail.com>
2025-03-19 13:09:38 -0400

    Quick test

--- driver/drive.py
+++ driver/drive.py
@@ -39,12 +39,12 @@ def stop():
 
 def turn(turn_right=True, error=0):
     if turn_right:
-        right_motor_speed = SPEED * ((K_differential * error) / 240)
+        right_motor_speed = SPEED * ((K_differential * error) / 160)
         left_motor_speed  = SPEED
         # print("motor speed, right turn :", right_motor_speed)
 
     else:
-        left_motor_speed = SPEED * ((K_differential * error) / 240)
+        left_motor_speed = SPEED * ((K_differential * error) / 160)
         right_motor_speed  = SPEED
         # print("motor speed, left turn :", left_motor_speed)
        
