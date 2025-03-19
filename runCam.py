6383ef6a77bac466e67dc0f703140005d1165fcb
Rayan Ahmad <rayanthefirst@gmail.com>
2025-03-19 14:57:09 -0400

    Quick test

--- runCam.py
+++ runCam.py
@@ -1,5 +1,5 @@
 from camera.camera import Camera
 from threading import Thread
 cam = Camera(camera_id=0)
-cameraProcess = Thread(target=cam.start_detection, kwargs={"display": False})
+cameraProcess = Thread(target=cam.start_detection, kwargs={"display": True})
 cameraProcess.start()
