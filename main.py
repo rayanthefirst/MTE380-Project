0693e235e1a91727e08f6be814731d223569c11a
Kareem Mansour <ksmansou@uwaterloo.ca>
2025-03-20 19:34:37 -0400

    kjhg

--- main.py
+++ main.py
@@ -21,8 +21,8 @@ dt = 1 / cam.fps  # Time step based on camera frame rate
 max_error = cam.width / 2
 
 ki = 0  # Keeping integral disabled for now
-kp = (MAX_SPEED / max_error) * 0.8  # Increase KP for better response
-kd = 0.1  # Reduce KD to prevent stopping at turns
+kp = (MAX_SPEED / max_error) * 1  # Increase KP for better response
+kd = 100  # Reduce KD to prevent stopping at turns
 
 while True:
     if cam.isRedLineDetected:
