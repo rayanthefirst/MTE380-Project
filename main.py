cc131d03fd8bb2c62de9615d7f0f17ebd931c7a4
Kareem Mansour <ksmansou@uwaterloo.ca>
2025-03-20 18:40:04 -0400

    Testing PD control

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-
 from camera.camera import Camera
 from threading import Thread
 from driver.drive import *
-from utils import scale_input
 
 # SPEED IS RESTRICTED BETWEEN 0 AND 1
-# From voltage test
 MAX_SPEED = 0.2
 
 # From straight line test
@@ -19,14 +16,14 @@ integral = 0
 derivative = 0
 curr_error = 0
 prev_error = 0
-dt = 1/cam.fps
+dt = 1 / cam.fps  # Time step based on camera frame rate
 
-max_error = cam.width/2
-# max_derivative = cam.width/dt
+max_error = cam.width / 2
 
-kp = (MAX_SPEED / (max_error )) 
-ki = 0
-kd = float(input("Enter kp value: "))
+# PID Gains
+kp = MAX_SPEED / max_error  # Proportional gain
+ki = 0  # Keeping integral disabled for now
+kd = 0.01  # Small derivative gain (tune this value)
 
 while True:
     if cam.isRedLineDetected:
@@ -35,13 +32,13 @@ while True:
 
         if abs(curr_error) > error_threshold:
             print("Error large; adjusting turn.")
-            integral += curr_error * dt
+
+            # Compute PID terms
             derivative = (curr_error - prev_error) / dt
 
             p_out = kp * curr_error
-            i_out = ki * integral
             d_out = kd * derivative
-            output = p_out + i_out + d_out
+            output = p_out + d_out  # No integral term yet
 
             # Scale output to voltage
             speedDelta = min(MAX_SPEED / 2, abs(output))
@@ -49,24 +46,17 @@ while True:
             # Adjust PID Speed for right and left
             if curr_error > 0:
                 # If error is positive, turn right
-                speed_left = (MAX_SPEED/2) + speedDelta
-                speed_right = (MAX_SPEED/2) - speedDelta
+                speed_left = (MAX_SPEED / 2) + speedDelta
+                speed_right = (MAX_SPEED / 2) - speedDelta
             else:
                 # If error is negative, turn left
-                speed_left = (MAX_SPEED/2) - speedDelta
-                speed_right = (MAX_SPEED/2) + speedDelta
-            # print("Speed left: ", speed_left)
-            # print("Speed right: ", speed_right)
+                speed_left = (MAX_SPEED / 2) - speedDelta
+                speed_right = (MAX_SPEED / 2) + speedDelta
 
             drive(speedLeft=speed_left, speedRight=speed_right)
-            
         else:
-            integral = 0
             derivative = 0
-            # If error is small, drive forward.
-            drive(speedLeft=(MAX_SPEED/2), speedRight=(MAX_SPEED/2), forward=True)
-            # print('speed left: ', (MAX_SPEED/2))    
-        
+            drive(speedLeft=(MAX_SPEED / 2), speedRight=(MAX_SPEED / 2), forward=True)
     else:
         # No red detected; stop the motors.
         stop()
