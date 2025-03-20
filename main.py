6cf9ea6d9ef6b6ec956389fbf55004e5221491a4
Kareem Mansour <ksmansou@uwaterloo.ca>
2025-03-20 19:13:22 -0400

    values

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from threading import Thread
 from driver.drive import *
 
 # SPEED IS RESTRICTED BETWEEN 0 AND 1
-MAX_SPEED = 0.16
+MAX_SPEED = 0.165
 
 # From straight line test
 error_threshold = 50
