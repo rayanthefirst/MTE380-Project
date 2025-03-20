18e5bb689ff98126e712a68f28cd3d3b87b1119d
Rayan Ahmad <rayanthefirst@gmail.com>
2025-03-20 13:41:31 -0400

    Quick test

--- driver/drive.py
+++ driver/drive.py
@@ -11,7 +11,7 @@ right_motor = Motor(forward=12, backward=13)
 left_encoder = RotaryEncoder(a=9, b=10, max_steps=0)
 right_encoder = RotaryEncoder(a=17, b=18, max_steps=0)
 
-SPEED = 1
+SPEED = 0.75
 TURN_SPEED = 0.1
 K_differential = 0.01
 
