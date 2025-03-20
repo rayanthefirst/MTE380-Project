c88cdf1e8080062e38c7ef44e5bbe09842dd912f
Rayan Ahmad <rayanthefirst@gmail.com>
2025-03-20 11:52:27 -0400

    Quick test

--- main.py
+++ main.py
@@ -1,21 +1,16 @@
 
 from camera.camera import Camera
 from threading import Thread
-from time import sleep
 from driver.drive import *
+
 cam = Camera(camera_id=0)
 cameraProcess = Thread(target=cam.start_detection, kwargs={"display": True})
 cameraProcess.start()
-Kp = 1
-Ka = 1
+
 error_threshold = 50
 
 while True:
     if cam.isRedLineDetected:
-        print("Red line detected")
-
-        print(cam.curr_error)
-        
         if abs(cam.curr_error) < error_threshold:
             print("Error small; driving forward.")
             # If error is small, drive forward.
