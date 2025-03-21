670e062ee2a3ed990685614a5d4db1674c03e552
Rayan Ahmad <rayanthefirst@gmail.com>
2025-03-21 00:27:59 -0400

    Quick test

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ cam = Camera(camera_id=0)
 cameraProcess = Thread(target=cam.start_detection, kwargs={"display": False})
 cameraProcess.start()
 
-error_threshold = 50
+error_threshold = 35
 
 while True:
     if cam.isRedLineDetected:
