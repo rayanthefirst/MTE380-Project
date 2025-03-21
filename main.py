9e78359e8a7534c3618af3956a66d7dd3af01b30
Kareem Mansour <ksmansou@uwaterloo.ca>
2025-03-20 20:21:07 -0400

    lolll

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from threading import Thread
 from driver.drive import *
 
 # SPEED IS RESTRICTED BETWEEN 0 AND 1
-MAX_SPEED = 0.20
+MAX_SPEED = 0.23
 
 # From straight line test
 error_threshold = 50
@@ -40,7 +40,7 @@ while True:
             output = p_out + d_out 
 
             # Scale output to voltage
-            speedScalar = 4.5
+            speedScalar = 5.0
             speedDelta = min(MAX_SPEED / speedScalar, abs(output))
 
             # Adjust PID Speed for right and left
