156a69b43d07b2393f2a34db81bd55dbea224bbf
Kareem Mansour <ksmansou@uwaterloo.ca>
2025-03-20 22:12:18 -0400

    Quick test

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ while True:
             output = p_out + d_out 
 
             # Scale output to voltage
-            speedScalar = 4.0
+            speedScalar = 3.0
             speedDelta = min(MAX_SPEED / speedScalar, abs(output))
 
             # Adjust PID Speed for right and left
