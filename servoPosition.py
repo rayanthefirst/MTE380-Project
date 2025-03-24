fce7f94daa131fc6e7fe9146e196baad36217caf
Kareem Mansour <ksmansou@uwaterloo.ca>
2025-03-24 17:56:18 -0400

    Quick test

--- servoPosition.py
+++ servoPosition.py
@@ -16,7 +16,7 @@ try:
     while True:
         left_pos = left_servo.value  # range: -1 (min) to 1 (max)
         right_pos = right_servo.value
-        print(f"Left Servo: {left_servo:.2f} | Right Servo: {right_servo:.2f}")
+        print(f"Left Servo: {left_servo} | Right Servo: {right_servo}")
         time.sleep(0.5)
 
 except KeyboardInterrupt:
