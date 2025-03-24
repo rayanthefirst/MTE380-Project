5b690b621f71e19b0f63ab9d1ec7ff6387aa2949
Kareem Mansour <ksmansou@uwaterloo.ca>
2025-03-24 18:41:30 -0400

    Quick test

--- driver/servo.py
+++ driver/servo.py
@@ -1,30 +1,18 @@
-#left_servo = Servo(26, min_pulse_width=0.0005, max_pulse_width=0.0025)
-#right_servo = Servo(20, min_pulse_width=0.0005, max_pulse_width=0.0025)
-
-# driver/servo.py
-
 from gpiozero import Servo
 from time import sleep
 
-left_servo = None
-right_servo = None
-
-def init_servos():
-    global left_servo, right_servo
-    if left_servo is None or right_servo is None:
-        left_servo = Servo(26, min_pulse_width=0.0005, max_pulse_width=0.0025)
-        right_servo = Servo(20, min_pulse_width=0.0005, max_pulse_width=0.0025)
+# This uses default pin factory (RPi.GPIO)
+left_servo = Servo(26)
+right_servo = Servo(20)
 
 def open_arms():
-    init_servos()
     print("Opening arms...")
-    left_servo.value = 1.0
-    right_servo.value = -1.0
+    left_servo.min()
+    right_servo.max()
     sleep(1)
 
 def close_arms():
-    init_servos()
     print("Closing arms...")
-    left_servo.value = -1.0
-    right_servo.value = 1.0
+    left_servo.max()
+    right_servo.min()
     sleep(1)
