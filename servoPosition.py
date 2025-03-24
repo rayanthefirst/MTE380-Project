bce04ab4c9c56501665956ebfe5d303e45d2894c
Kareem Mansour <ksmansou@uwaterloo.ca>
2025-03-24 17:55:21 -0400

    Quick test

--- servoPosition.py
+++ servoPosition.py
@@ -7,6 +7,8 @@ import time
 left_servo = Servo(26)
 right_servo = Servo(20)
 
+left_servo.value = None  # no command yet
+right_servo.value = None
 
 print("Monitoring servo positions... (press Ctrl+C or q in window to quit)")
 
