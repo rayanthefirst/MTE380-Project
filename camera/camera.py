6ddcb9c6f9d0bceab945f4402fb698e20edbb7b0
Kareem Mansour <ksmansou@uwaterloo.ca>
2025-03-24 18:53:55 -0400

    Quick test

--- camera/camera.py
+++ camera/camera.py
@@ -31,7 +31,7 @@ class Camera:
         self.latest_frame = None
         self.sees_blue = False  # <- NEW FLAG
 
-    def start_detection(self, display=False, video_filename=None):
+    def start_detection(self, display=True, video_filename=None):
         while True:
             ret, frame = self.cap.read()
             if not ret:
