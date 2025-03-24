791f52b68843f54b7fb2bd08a90d79ce0afb9ce9
Kareem Mansour <ksmansou@uwaterloo.ca>
2025-03-24 18:03:20 -0400

    Quick test

--- servoPosition.py
+++ servoPosition.py
@@ -17,8 +17,8 @@ try:
         left_input = input("Enter position for left servo (-1 to 1): ")
         right_input = input("Enter position for right servo (-1 to 1): ")
         try:
-            left_pos = left_servo.value  # range: -1 (min) to 1 (max)
-            right_pos = right_servo.value
+            left_pos = float(left_input)
+            right_pos = float(right_input)
             if -1 <= left_pos <= 1 and -1 <= right_pos <= 1:
                 left_servo.value = left_pos
                 right_servo.value = right_pos
