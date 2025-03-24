455d191bbc724043d114a464163501c84d74eec3
Kareem Mansour <ksmansou@uwaterloo.ca>
2025-03-24 19:42:52 -0400

    Quick test

--- driver/servo.py
+++ driver/servo.py
@@ -1,17 +1,27 @@
 from gpiozero import Servo
 from time import sleep
 
-# This uses default pin factory (RPi.GPIO)
-left_servo = Servo(26)
-right_servo = Servo(20)
+# Lazy-loaded servos
+left_servo = None
+right_servo = None
+
+def initialize_servos():
+    global left_servo, right_servo
+    if left_servo is None or right_servo is None:
+        print("Initializing servos...")
+        left_servo = Servo(26)
+        right_servo = Servo(20)
+        sleep(0.5)  # Give them time to settle
 
 def open_arms():
+    initialize_servos()
     print("Opening arms...")
     left_servo.value = -1.0
     right_servo.value = 1.0
     sleep(1)
 
 def close_arms():
+    initialize_servos()
     print("Closing arms...")
     left_servo.value = 1.0
     right_servo.value = -1.0
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from camera.camera import Camera
 from driver.drive import drive, stop, turn
-# from driver.servo import open_arms, close_arms 
+from driver.servo import open_arms, close_arms 
 
 import cv2
 import numpy as np
