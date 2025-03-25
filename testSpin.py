14183e656078c8813814bbcf66a91ca3c11f799f
Kareem Mansour <ksmansou@uwaterloo.ca>
2025-03-25 12:39:38 -0400

    Quick test

--- testSpin.py
+++ testSpin.py
@@ -1,14 +1,9 @@
 from camera.camera import Camera
-from driver.drive import stop
-from gpiozero import Motor
+from driver.drive import left_motor, right_motor, stop
 import threading
 from time import sleep
 
-# Access motor pins directly
-left_motor = Motor(forward=14, backward=15)
-right_motor = Motor(forward=12, backward=13)
-
-# Start red detection in a thread
+# Start camera red line detection
 cam = Camera(camera_id=0)
 cameraThread = threading.Thread(target=cam.start_detection, kwargs={"display": False})
 cameraThread.start()
@@ -17,15 +12,16 @@ print("Spinning in place (clockwise) until red line is detected...")
 
 try:
     while not cam.isRedLineDetected:
+        # In-place clockwise spin: left motor backward, right motor forward
         left_motor.backward(speed=0.1)
         right_motor.forward(speed=0.1)
-        sleep(0.2)
+        sleep(0.1)
 
     stop()
     print("Red line detected! Stopped.")
 
 except KeyboardInterrupt:
-    print("Interrupted. Stopping motors.")
+    print("Interrupted. Stopping.")
     stop()
 
 finally:
