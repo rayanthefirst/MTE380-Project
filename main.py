0b9da2fc28e2a6b9c4f2c07e91386be136a4716c
Kareem Mansour <ksmansou@uwaterloo.ca>
2025-03-25 13:00:32 -0400

    Quick test

--- main.py
+++ main.py
@@ -67,8 +67,8 @@ while True:
 
         print("Rotating in place until red line is found...")
         while not cam.isRedLineDetected:
-            left_motor.forward(speed=0.2)
-            right_motor.forward(speed=0.1)
+            left_motor.forward(speed=0.02)
+            right_motor.forward(speed=0.01)
             sleep(0.2)
 
         stop()
