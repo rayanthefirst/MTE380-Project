1b1f1b00f1639bda75306c5f55b0e4842ca9646d
Kareem Mansour <ksmansou@uwaterloo.ca>
2025-03-27 19:28:57 -0400

    Quick test

--- driver/drive.py
+++ driver/drive.py
@@ -11,8 +11,8 @@ right_motor = Motor(forward=12, backward=13)
 left_encoder = RotaryEncoder(a=9, b=10, max_steps=0)
 right_encoder = RotaryEncoder(a=17, b=18, max_steps=0)
 
-SPEED = 0.10
-TURN_SPEED = 0.08
+SPEED = 0.09
+TURN_SPEED = 0.09
 kp = 0.25
 kd = 0.30
 ki = 0.23
