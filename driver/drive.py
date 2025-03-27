eef69afe69d73ef793ba56316d9162231ebf405f
Kareem Mansour <ksmansou@uwaterloo.ca>
2025-03-27 19:33:41 -0400

    Quick test

--- driver/drive.py
+++ driver/drive.py
@@ -14,8 +14,8 @@ right_encoder = RotaryEncoder(a=17, b=18, max_steps=0)
 SPEED = 0.09
 TURN_SPEED = 0.09
 kp = 0.25
-kd = 0.30
-ki = 0.25
+kd = 0.32
+ki = 0.22
 
 last_error = 0.0
 integral_error = 0.0
