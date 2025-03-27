18ef586a0fc8d708a9a4f7020758d2bc87746b17
Kareem Mansour <ksmansou@uwaterloo.ca>
2025-03-26 21:29:11 -0400

    Quick test

--- driver/servo.py
+++ driver/servo.py
@@ -16,8 +16,8 @@ def close_arms():
     print("Closing arms...")
     left = Servo(26)
     right = Servo(20)
-    left.value = 1.0
-    right.value = -1.0
+    left.max()      # Equivalent to left.value = 1.0
+    right.min()     # Equivalent to right.value = -1.0
     sleep(1)
     left.detach()
     right.detach()
